3e9621c9052e2fb4d920fe95489025463cf3fdcf
rodrigoross <rodrigo.ross.comp@gmail.com>
2021-12-28 17:35:15 -0400

    chore: cria mapeamento do form com variaveis

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const inputEndereco = document.getElementById("endereco")! as HTMLInputElement;
+
+function buscaEnderecoHandler(event: Event) {
+  event.preventDefault();
+
+  const endereco = inputEndereco.value;
+}
+
+form.addEventListener("submit", buscaEnderecoHandler);
